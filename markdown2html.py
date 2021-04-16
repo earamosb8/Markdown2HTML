bf8f3716b42303d493cb8a364ef2e60c5e51611f
earamosb8 <earamosb8@gmail.com>
2021-04-16 16:28:19 -0500

    add markdown2html

---
+++ markdown2html.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that takes 2 arguments"""
+import sys
+import os.path
+
+
+if __name__ == "__main__":
+    """not execute the code when imported"""
+    lenght = len(sys.argv)
+    if lenght < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+    elif os.path.exists(sys.argv[1]) is False:
+        sys.stderr.write("Missing " + sys.argv[1])
+        sys.exit(1)
+    else:
+        sys.exit(0)#!/usr/bin/python3
+"""script that takes 2 arguments"""
+import sys
+import os.path
+
+
+if __name__ == "__main__":
+    """not execute the code when imported"""
+    lenght = len(sys.argv)
+    if lenght < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+    elif os.path.exists(sys.argv[1]) is False:
+        sys.stderr.write("Missing " + sys.argv[1])
+        sys.exit(1)
+    else:
+        sys.exit(0)
